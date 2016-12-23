ccdc41e34cf7c815fa9a8962f977c0a3ab4347ff
Frank Duckart <duckart@computer.org>
2016-12-22 19:30:05 -1000

    Various bug fixes.

--- src/main/java/edu/hawaii/its/holiday/controller/HomeController.java
+++ src/main/java/edu/hawaii/its/holiday/controller/HomeController.java
@@ -1,42 +1,25 @@
 package edu.hawaii.its.holiday.controller;
 
-import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import edu.hawaii.its.holiday.service.HolidayService;
-import edu.hawaii.its.holiday.type.Holiday;
 
 @Controller
 public class HomeController {
 
     private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 
-    @Autowired
-    private HolidayService holidayService;
-
-    @Value("${welcome.message:test}")
-    private String message = "Hello World";
-
     @RequestMapping(value = { "", "/", "/home" }, method = { RequestMethod.GET })
-    public String main(Map<String, Object> model, Locale locale) {
-        logger.info("User at main. The client locale is {}.", locale);
+    public String home(Map<String, Object> model, Locale locale) {
+        logger.info("User at home. The client locale is {}.", locale);
 
-        model.put("message", this.message);
-        List<Holiday> holidays = holidayService.findHolidays();
-        model.put("holidays", holidays);
-
-        return "main";
-
+        return "home";
     }
 
     @RequestMapping(value = "/contact", method = RequestMethod.GET)
