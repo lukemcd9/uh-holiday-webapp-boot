48028adef76807ba7078ed1b549f35073d19d690
Frank Duckart <duckart@computer.org>
2016-12-27 15:48:13 -1000

    Add Employee tests.

---
+++ src/test/java/edu/hawaii/its/holiday/type/EmployeeTest.java
@@ -0,0 +1,36 @@
+package edu.hawaii.its.holiday.type;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class EmployeeTest {
+
+    private Employee employee;
+
+    @Before
+    public void setUp() {
+        employee = new Employee();
+    }
+
+    @Test
+    public void construction() {
+        assertNotNull(employee);
+    }
+
+    @Test
+    public void setters() {
+        assertNotNull(employee);
+        assertNull(employee.getUhNumber());
+        assertNotNull(employee.toString());
+
+        employee.setUhNumber(12345678L);
+        assertThat(employee.getUhNumber(), equalTo(12345678L));
+        assertThat(employee.toString(), containsString("uhNumber=12345678"));
+    }
+}
