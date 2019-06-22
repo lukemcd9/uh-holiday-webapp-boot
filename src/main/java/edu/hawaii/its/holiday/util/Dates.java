1a8ff4bc1b0a876242d4f0748dffc4ee070b8b5c
lukemcd9 <lukemcd97@gmail.com>
2019-06-21 14:03:52 -1000

    Added date comparison and tests. Fixed typos.

--- src/main/java/edu/hawaii/its/holiday/util/Dates.java
+++ src/main/java/edu/hawaii/its/holiday/util/Dates.java
@@ -8,7 +8,9 @@ import java.time.Month;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.TemporalUnit;
 import java.util.Date;
 
 public final class Dates {
@@ -18,7 +20,11 @@ public final class Dates {
 
     // Private constructor; prevent instantiation.
     private Dates() {
-        // Emtpy.
+        // Empty.
+    }
+
+    public static long compareDates(LocalDate firstDate, LocalDate secondDate) {
+        return ChronoUnit.DAYS.between(firstDate, secondDate);
     }
 
     public static LocalDate newLocalDate() {
--- src/test/java/edu/hawaii/its/holiday/util/DatesTest.java
+++ src/test/java/edu/hawaii/its/holiday/util/DatesTest.java
@@ -60,6 +60,13 @@ public class DatesTest {
     }
 
     @Test
+    public void compareDates() {
+        LocalDate date1 = Dates.newLocalDate(2019, Month.JUNE, 21);
+        LocalDate date2 = Dates.newLocalDate(2009, Month.JUNE, 23);
+        assertEquals(Dates.compareDates(date1, date2), 10L);
+    }
+
+    @Test
     public void newLocalDate() {
         assertNotNull(Dates.newLocalDate());
     }
