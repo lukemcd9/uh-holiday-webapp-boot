8534cdf632010b9b139526c685c3e138d8b184fd
lukemcd9 <34250936+lukemcd9@users.noreply.github.com>
2019-07-01 10:50:35 -1000

    Using external MySQL database and fixed data loading into home table.

--- src/main/java/edu/hawaii/its/holiday/configuration/SpringBootWebApplication.java
+++ src/main/java/edu/hawaii/its/holiday/configuration/SpringBootWebApplication.java
@@ -5,11 +5,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
 public class SpringBootWebApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         SpringApplication.run(SpringBootWebApplication.class);
     }
 
