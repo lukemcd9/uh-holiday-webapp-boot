41770f3471da0c7c015670de433b994e9cb97884
lukemcd9 <lukemcd97@gmail.com>
2019-07-22 10:19:35 -1000

    Added custom properties

--- src/main/java/edu/hawaii/its/holiday/configuration/AppConfigTest.java
+++ src/main/java/edu/hawaii/its/holiday/configuration/AppConfigTest.java
@@ -6,29 +6,30 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.ComponentScan.Filter;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.FilterType;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-@Profile(value = { "dev", "default", "localhost" })
+@Profile(value = { "test" })
 @Configuration
-@ComponentScan(basePackages = { "edu.hawaii.its.holiday" },
-        excludeFilters = { @Filter(type = FilterType.ANNOTATION, value = Configuration.class) })
-@EntityScan(basePackages = { "edu.hawaii.its.holiday.type" })
+@ComponentScan(basePackages = "edu.hawaii.its.holiday")
 @EnableJpaRepositories(basePackages = { "edu.hawaii.its.holiday.repository" })
+@PropertySources({
+        @PropertySource(value = "classpath:custom.properties"),
+        @PropertySource(value = "file:${user.home}/.${user.name}-conf/holidays-overrides.properties",
+                ignoreResourceNotFound = true)
+})
+@EntityScan(basePackages = { "edu.hawaii.its.holiday.type" })
 @EnableTransactionManagement
-@PropertySource("classpath:custom.properties")
-public class WebConfig {
+public class AppConfigTest {
 
-    private static final Log logger = LogFactory.getLog(WebConfig.class);
+    private static Log logger = LogFactory.getLog(AppConfigTest.class);
 
     @PostConstruct
     public void init() {
-        logger.info("AppConfig init");
-
+        logger.info("AppConfigTest init");
     }
 }
